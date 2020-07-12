413d22bf868b2898b78d2576205e49f8da27fbdf
bennerg2 <bgurrola.btg@gmail.com>
2020-07-11 23:27:01 -0600

    completed 6kyu kata - string manipulation

---
+++ python/2020-07-11.py
@@ -0,0 +1,23 @@
+# Provided Instructions:
+# In DNA strings, symbols "A" and "T" are complements
+# of each other, as "C" and "G". You have function with
+# one side of the DNA (string, except for Haskell); you
+# need to get the other complementary side. DNA strand
+# is never empty or there is no DNA at all (again, 
+# except for Haskell).
+
+def DNA_strand(dna):
+    answer = ""
+    for base in dna:
+        if base == 'A':
+            answer += 'T'
+        elif base == 'T':
+            answer += 'A'
+        elif base == 'G':
+            answer += 'C'
+        else: # base == 'C'
+            answer += 'G'
+    return answer
+
+print(DNA_strand("ATTGC"))
+print(DNA_strand("GTAT"))
